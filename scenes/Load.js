118e9ae1cf1e0aae8a9be03eb209bad4eed3b03d
gvsalazar99 <gvsalaza@ucsc.edu>
2020-04-28 00:11:50 -0700

    new menu screen

--- scenes/Load.js
+++ scenes/Load.js
@@ -10,7 +10,7 @@ class Load extends Phaser.Scene {
         this.load.image('groundScroll', 'ground.png');
         this.load.image('dragonGirl', 'dragongirl.png');
         this.load.image('background', 'background pink stars.png');
-        this.load.image('menu', 'menu screen.png');
+        this.load.image('menu', 'menu_screen_new.png');
         this.load.image('obstacle', 'dragonGirl.png');
         this.load.image('upGem', 'upwards gem.png');
         this.load.image('downGem', 'downwards gem.png');
@@ -19,6 +19,8 @@ class Load extends Phaser.Scene {
         this.load.image('icicle', 'icicle.png');
         this.load.image('planet', 'planet.png');
         this.load.image('moon', 'moon.png');
+        
+
 
 
         //this.load.spritesheet('fly', 'Dragongirlspritesheet.PNG', {frameWidth:120, frameHeight:120, startFrame:0, endFrame:5});
@@ -37,7 +39,7 @@ class Load extends Phaser.Scene {
         music.play( {loop:true} );
 
         //add menu image
-        this.add.image(0, 0, 'menu').setOrigin(0).setScale(.32, .3);
+        this.add.image(-2, -20, 'menu').setOrigin(0).setScale(1.35, 1.35);
 
         //space bar as input
         spaceBar= this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
